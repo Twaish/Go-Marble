b7163c2068b94f50035cb247f20865576c615d8c
Twaish <axiomeverything@gmail.com>
2025-03-10 11:46:41 +0100

    Added changes to `PlayerController` - Added conditional player configs for specific surface materials - Bounciness now modifies the players physics material - Moved movement logic when grounded and not moving into its own function - Added custom gravity

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -13,7 +14,7 @@ public class PlayerController : MonoBehaviour {
 
   [Header("Environment Control")]
   [SerializeField, Tooltip("The scale of gravity applied to the player")]
-  private float gravityScale = 1f;
+  private float gravityScale = 2f;
   [SerializeField, Tooltip("The force applied to the player's movement while in midair")]
   private float airControlForce = 15f;
   [SerializeField, Tooltip("How quickly the player can rotate while in midair")]
@@ -45,77 +46,126 @@ public class PlayerController : MonoBehaviour {
   [SerializeField]
   private float shakeDuration = 1f;
   [SerializeField, Tooltip("Minimum collision magnitude before shaking camera")]
-  private float shakeImpactThreshold = 20f;
-  
+  private float shakeImpactThreshold = 35f;
+
+  [Header("Default Conditions")]
+  [SerializeField]
+  private float defaultSpeed = 45f;
+  [SerializeField]
+  private float defaultTurnSpeed = 5f;
+  [SerializeField]
+  private float defaultDecelerationRate = 1f;
+  [SerializeField]
+  private float defaultTorque = 1f;
+  [SerializeField, Tooltip("How bouncy the player is when colliding with surfaces")]
+  private float defaultBounciness = .8f;
+
+  [Header("Sand Conditions")]
+  [SerializeField] 
+  private float sandSpeed = 30f;
+  [SerializeField] 
+  private float sandTurnSpeed = 3.5f;
+  [SerializeField] 
+  private float sandDecelerationRate = 4f;
+  [SerializeField] 
+  private float sandTorque = 1f;
+  [SerializeField]
+  private float sandBounciness = 0f;
+
+  [Header("Ice Conditions")]
+  [SerializeField] 
+  private float iceSpeed = 15f;
+  [SerializeField] 
+  private float iceTurnSpeed = 1f;
+  [SerializeField] 
+  private float iceDecelerationRate = 0.2f;
+  [SerializeField] 
+  private float iceTorque = 5f;
+  [SerializeField]
+  private float iceBounciness = .8f;
+
+  [Header("Development")]
+  [SerializeField]
+  private bool debugRays;
+
   private Rigidbody rb;
-  private float movementX;
-  private float movementY;
+  private SphereCollider sphereCollider;
   private CameraShaker cameraShaker;
   private Camera cameraComponent;
-  private bool isGrounded;
+  private Vector3 gravityDirection;
+  private List<GravityDirection> gravityDirections;
+  private float movementX;
+  private float movementY;
   private float lastJumpTime = -Mathf.Infinity;
-  
-  void Start() {
+  private bool isGrounded;
+
+  void Awake() {
     cameraComponent = mainCamera.GetComponent<Camera>();
     cameraShaker = mainCamera.GetComponent<CameraShaker>();
+    sphereCollider = GetComponent<SphereCollider>();
     rb = GetComponent<Rigidbody>();
+  }
 
-    rb.linearDamping = 0;
+  void Start() {
+    gravityDirections = new();
     rb.angularDamping = 0;
+    rb.linearDamping = 0;
+    sphereCollider.sharedMaterial.bounciness = bounciness;
   }
 
   void FixedUpdate() {
     Vector3 movement = GetCameraRelativeMovement();
     UpdateFOV(movement);
 
+    // HandleSurfaceAlignment();
+
     CheckGroundStatus();
 
     if (isGrounded) {
-      HandleGroundMovement(movement);
-    } else {
+      if (movement.magnitude > 0) {
+        HandleGroundMovement(movement);
+      } else {
+        HandleIdleMovement();
+      }
+    } else if (movement.magnitude > 0) {
       HandleAirMovement(movement);
     }
 
+    UpdateGravityDirection();
     UpdateGravity();
     EnforceSpeedLimit();
     HandleJump();
   }
 
   void HandleGroundMovement(Vector3 movement) {
-    if (movement.magnitude > 0) {
-      Vector3 movementForce = movement * speed;
-      rb.AddForce(movementForce, ForceMode.Force);
+    Vector3 movementForce = movement * speed;
+    rb.AddForce(movementForce, ForceMode.Force);
 
-      Quaternion targetRotation = Quaternion.LookRotation(movement);
-      transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.deltaTime);
-    }
+    Quaternion targetRotation = Quaternion.LookRotation(movement);
+    transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, turnSpeed * Time.fixedDeltaTime);
+  }
 
+  void HandleIdleMovement() {
     // Decrease velocity when no input is applied
-    if (movement.magnitude <= 0) {
-      Vector3 horizontalVelocity = new(rb.linearVelocity.x, 0, rb.linearVelocity.z);
-      horizontalVelocity = Vector3.Lerp(horizontalVelocity, Vector3.zero, decelerationRate * Time.fixedDeltaTime);
-      
-      rb.linearVelocity = new Vector3(
-        horizontalVelocity.x,
-        rb.linearVelocity.y,
-        horizontalVelocity.z
-      );
-    }
+    Vector3 horizontalVelocity = new(rb.linearVelocity.x, 0, rb.linearVelocity.z);
+    horizontalVelocity = Vector3.Lerp(horizontalVelocity, Vector3.zero, decelerationRate * Time.fixedDeltaTime);
+    
+    rb.linearVelocity = new Vector3(
+      horizontalVelocity.x,
+      rb.linearVelocity.y,
+      horizontalVelocity.z
+    );
     
     // Decrease angular velocity when not moving
-    if (movement.magnitude <= 0) {
-      rb.angularVelocity = Vector3.Lerp(rb.angularVelocity, Vector3.zero, decelerationRate * Time.fixedDeltaTime);
-    }
+    rb.angularVelocity = Vector3.Lerp(rb.angularVelocity, Vector3.zero, decelerationRate * Time.fixedDeltaTime);
   }
 
   void HandleAirMovement(Vector3 movement) {
     // Add rotation and reduced velocity in midair
-    if (movement.magnitude > 0) {
-      rb.AddForce(movement * airControlForce, ForceMode.Force);
+    rb.AddForce(movement * airControlForce, ForceMode.Force);
 
-      Vector3 rotationAxis = Vector3.Cross(Vector3.up, movement);
-      rb.AddTorque(rotationAxis * airRotationSpeed, ForceMode.Force);
-    }
+    Vector3 rotationAxis = Vector3.Cross(Vector3.up, movement);
+    rb.AddTorque(rotationAxis * airRotationSpeed, ForceMode.Force);
   }
   
   void EnforceSpeedLimit() {
@@ -131,17 +181,38 @@ public class PlayerController : MonoBehaviour {
   }
 
   void CheckGroundStatus() {
-    Debug.DrawRay(transform.position, Vector3.down * groundDetectionRange, Color.green);
-    isGrounded = Physics.Raycast(transform.position, Vector3.down, groundDetectionRange, groundMask);
+    if (debugRays) {
+      Debug.DrawRay(transform.position, gravityDirection.normalized * groundDetectionRange, Color.blue);
+    }
+    isGrounded = Physics.Raycast(transform.position, gravityDirection.normalized, groundDetectionRange, groundMask);
   }
 
   void UpdateFOV(Vector3 movement) {
     float targetFOV = Mathf.Lerp(baseFOV, maxFOV, movement.magnitude);
-    cameraComponent.fieldOfView = Mathf.Lerp(cameraComponent.fieldOfView, targetFOV, Time.deltaTime);
+    cameraComponent.fieldOfView = Mathf.Lerp(cameraComponent.fieldOfView, targetFOV, Time.fixedDeltaTime);
+  }
+
+  void UpdateGravityDirection() {
+    gravityDirection = Physics.gravity;
+
+    // https://github.com/ThermiteFe8/Custom-Gravity-Physics-Unity/blob/main/Assets/Scripts/GravityUtils/CustomGravityAffected.cs
+    if (gravityDirections.Count > 0) {
+      int highestPriority = -100;
+      Vector3 gravity = Vector3.zero;
+      for(int i = 0; i < gravityDirections.Count; i++) {
+        GravityDirection fallingRequest = gravityDirections[i];
+        if (fallingRequest.priority > highestPriority) {
+          gravity = fallingRequest.gravity;
+          highestPriority = fallingRequest.priority;
+        }
+      }
+      gravityDirection = gravity;
+    }
+    gravityDirections.Clear();
   }
 
   void UpdateGravity() {
-    rb.linearVelocity += gravityScale * rb.mass * Time.fixedDeltaTime * Physics.gravity;
+    rb.linearVelocity += gravityScale * rb.mass * Time.fixedDeltaTime * gravityDirection;
   }
   
   // Rotate movement vector inline with camera rotation
@@ -168,7 +239,7 @@ public class PlayerController : MonoBehaviour {
     ) {
       rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0, rb.linearVelocity.z);
       
-      rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+      rb.AddForce(-1 * jumpForce * gravityDirection.normalized, ForceMode.Impulse);
 
       lastJumpTime = Time.time;
       isGrounded = false;
@@ -184,12 +255,46 @@ public class PlayerController : MonoBehaviour {
 
   void OnCollisionEnter(Collision collision) {
     float impactForce = collision.relativeVelocity.magnitude;
-    
+
+    ApplyMaterialConditions(collision.gameObject);
+
     if (impactForce > shakeImpactThreshold) {
       float intensity = Mathf.Clamp(impactForce / 40f, 0.05f, 1f) * shakeIntensity;
       float duration = Mathf.Clamp(impactForce / 50f, 0.1f, 0.5f) * shakeDuration;
       
       StartCoroutine(cameraShaker.Shake(duration, intensity));
     }
+  }
+
+  void ApplyMaterialConditions(GameObject gameObject) {
+    if (gameObject.CompareTag("Sand")) {
+      speed = sandSpeed;
+      turnSpeed = sandTurnSpeed;
+      decelerationRate = sandDecelerationRate;
+      sphereCollider.sharedMaterial.bounciness = sandBounciness;
+    } else if (gameObject.CompareTag("Ice")) {
+      speed = iceSpeed;
+      turnSpeed = iceTurnSpeed;
+      decelerationRate = iceDecelerationRate;
+      sphereCollider.sharedMaterial.bounciness = iceBounciness;
+    } else {
+      speed = defaultSpeed;
+      turnSpeed = defaultTurnSpeed;
+      decelerationRate = defaultDecelerationRate;
+      sphereCollider.sharedMaterial.bounciness = defaultBounciness;
+    }
+  }
+
+  // void HandleSurfaceAlignment() {
+  //   if (Physics.Raycast(transform.position, gravityDirection, out RaycastHit hit, groundMask) && hit.collider.gameObject.layer == LayerMask.NameToLayer("AlignableSurface")) {
+  //     gravityDirection = -hit.normal * 9.82f;      
+    
+  //     Quaternion rotation = Quaternion.FromToRotation(Vector3.up, hit.normal) * transform.rotation;
+  //     transform.rotation = Quaternion.Slerp(transform.rotation, rotation, 5f * Time.fixedDeltaTime);
+  //   }
+  // }
+  
+  public void AddGravityDirection(GravityDirection gravityDirection) {
+    gravityDirections.Add(gravityDirection);
   }
 }
