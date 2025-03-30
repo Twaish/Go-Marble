7011177f0e8f25c61ae976b7fc73f6a95044ec0d
Twaish <axiomeverything@gmail.com>
2025-03-30 20:46:33 +0200

    Change marble preview movement
    - MarblePreview now moves left and right from its starting position

--- Assets/Scripts/Player/MarblePreviewMovement.cs
+++ Assets/Scripts/Player/MarblePreviewMovement.cs
@@ -2,21 +2,39 @@ using UnityEngine;
 
 public class MarblePreviewMovement : MonoBehaviour {
   [SerializeField]
-  private float speed = 15f;
+  private float speed = 50f;
   [SerializeField]
   private float maxSpeed = 10f;
+  [SerializeField]
+  private float maxDistance = 400f;
   
   private Rigidbody rb;
+  private Vector3 startPosition;
+  private float rightBoundary;
+  private float leftBoundary;
+  private bool movingRight = true;
 
   void Awake() {
     rb = GetComponent<Rigidbody>();
+    startPosition = transform.position;
+    rightBoundary = startPosition.x + maxDistance;
+    leftBoundary = startPosition.x - maxDistance;
   }
 
   void FixedUpdate() {
-    rb.AddForce(4 * speed * Time.fixedDeltaTime * Vector3.right, ForceMode.Force);
-    // if (rb.linearVelocity.magnitude > maxSpeed) {
-    //   Vector3 excessVelocity = rb.linearVelocity.normalized * (rb.linearVelocity.magnitude - maxSpeed);
-    //   rb.AddForce(-excessVelocity * 2, ForceMode.Force);
-    // }
+    int direction = movingRight ? 1 : -1;
+    Vector3 movementForce = direction * 4 * speed * Time.fixedDeltaTime * Vector3.right;
+    rb.AddForce(movementForce, ForceMode.Force);
+
+    if (rb.linearVelocity.magnitude > maxSpeed) {
+      rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+    }
+
+    if (
+      (movingRight && transform.position.x >= rightBoundary) ||
+      (!movingRight && transform.position.x <= leftBoundary)
+    ) {
+      movingRight = !movingRight;
+    }
   }
 }
