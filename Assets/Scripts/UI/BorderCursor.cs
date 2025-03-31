33b0cc95fcde7e299c48f26d806274fbb1ccdf34
Twaish <axiomeverything@gmail.com>
2025-03-31 14:25:19 +0200

    Added navigation cursor

---
+++ Assets/Scripts/UI/BorderCursor.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+
+public class BorderCursor : MonoBehaviour {
+  [SerializeField]
+  private float pulseSpeed = 1f;
+  [SerializeField]
+  private float pulseSize = 1.2f;
+  [SerializeField]
+  private float borderThickness = 5f;
+  [SerializeField]
+  private float positionLerpSpeed = 10f;
+
+  private RectTransform imageTransform;
+  private GameObject currentFocusedUI;
+  private Vector3 targetPosition;
+  private float pulseTime;
+  private Image image;
+
+  private void Start() {
+    image = GetComponent<Image>();
+    imageTransform = image.GetComponent<RectTransform>();
+    targetPosition = imageTransform.position;
+    StartCoroutine(PulsateBorder());
+  }
+
+  private void FixedUpdate() {
+    GameObject focusedUI = EventSystem.current.currentSelectedGameObject;
+    if (focusedUI == null) return;
+    if (focusedUI == currentFocusedUI) {
+      imageTransform.position = Vector3.Lerp(imageTransform.position, targetPosition, Time.fixedDeltaTime * positionLerpSpeed);
+      return;
+    }
+
+    currentFocusedUI = focusedUI;
+    UpdateBorderPosition(focusedUI);
+  }
+
+  private void UpdateBorderPosition(GameObject focusedUI) {
+    if (!focusedUI.TryGetComponent<RectTransform>(out var focusedRect)) return;
+    Vector2 centerPosition = focusedRect.TransformPoint(focusedRect.rect.center);
+
+    targetPosition = centerPosition;
+    imageTransform.sizeDelta = focusedRect.sizeDelta + new Vector2(borderThickness, borderThickness);
+  }
+
+  private IEnumerator PulsateBorder() {
+    Vector3 initialScale = imageTransform.localScale;
+
+    while (true) {
+      pulseTime += Time.deltaTime * pulseSpeed;
+      float smoothedPulse = Mathf.SmoothStep(1f, pulseSize, Mathf.PingPong(pulseTime, 1f));
+      imageTransform.localScale = initialScale * smoothedPulse;
+
+      yield return null;
+    }
+  }
+
+}
