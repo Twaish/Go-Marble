0b75364f2809ac1d1480bf9c193d66bdf8bcb8ce
Twaish <axiomeverything@gmail.com>
2025-04-28 12:48:23 +0200

    Moved `PowerUpManager` into `./PowerUps`. Implemented power up system. Added SpeedBoost power up

---
+++ Assets/Scripts/PowerUps/SpeedBoostPowerUp.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SpeedBoost", menuName = "Powerups/SpeedBoost")]
+public class SpeedBoostPowerup : BasePowerUp {
+  public float BurstStrength = 20f;
+
+  public override void Effect(PlayerController player) {
+    player.StartCoroutine(SpeedBoost(player));
+  }
+
+  private IEnumerator SpeedBoost(PlayerController player) {
+    Vector3 direction = player.GetComponent<Rigidbody>().linearVelocity.normalized; 
+
+    player.GetComponent<Rigidbody>().AddForce(direction * BurstStrength, ForceMode.VelocityChange);
+
+    OnActivate(player);
+    yield return null;
+    OnExpire(player);
+  }
+
+  public override void OnActivate(PlayerController player) {
+    Debug.Log($"{Name} Activated!");
+  }
+
+  public override void OnExpire(PlayerController player) {
+    Debug.Log($"{Name} Expired!");
+  }
+}
