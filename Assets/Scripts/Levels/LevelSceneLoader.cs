733e9e1e81ad4c968fc027a7811b9c006abd9876
Twaish <axiomeverything@gmail.com>
2025-05-15 13:05:40 +0200

    Added `LevelSceneLoader`
    - Added `LevelSceneLoader` to handle async scene loading

---
+++ Assets/Scripts/Levels/LevelSceneLoader.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSceneLoader : MonoBehaviour {
+  public Coroutine LoadScene(string sceneName, Action onLoaded = null) {
+    return StartCoroutine(LoadRoutine());
+    IEnumerator LoadRoutine() {
+      AsyncOperation op = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+      yield return new WaitUntil(() => op.isDone);
+      onLoaded?.Invoke();
+    }
+  }
+
+  public Coroutine UnloadScene(string sceneName, Action onUnloaded = null) {
+    return StartCoroutine(UnloadRoutine());
+    IEnumerator UnloadRoutine() {
+      AsyncOperation op = SceneManager.UnloadSceneAsync(sceneName);
+      yield return new WaitUntil(() => op.isDone);
+      onUnloaded?.Invoke();
+    }
+  }
+}
