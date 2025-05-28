50892133130f36604862fd8c744156ac03c930c6
Twaish <axiomeverything@gmail.com>
2025-05-29 00:40:05 +0200

    Separated gameplay HUD controlling from GameplayManager

--- Assets/GameplayManager.cs
+++ Assets/GameplayManager.cs
@@ -1,13 +1,17 @@
+using System.Collections;
 using UnityEngine;
 
 public class GameplayManager : MonoBehaviour {
   [SerializeField] private TimerManager timerManager;
   [SerializeField] private NavigationManager navigationManager;
+  [SerializeField] private GameObject completeLevelFocus;
+  [SerializeField] private GameObject pausedLevelFocus;
 
   private void Awake() {
     LevelManager.instance.OnLevelStarted += HandleLevelStarted;
     LevelManager.instance.OnLevelEnded += HandleLevelEnded;
     LevelManager.instance.OnLevelPaused += HandleLevelPaused;
+    LevelManager.instance.OnLevelResumed += HandleLevelResumed;
   }
 
   private void OnDestroy() {
@@ -15,26 +19,46 @@ public class GameplayManager : MonoBehaviour {
       LevelManager.instance.OnLevelStarted -= HandleLevelStarted;
       LevelManager.instance.OnLevelEnded -= HandleLevelEnded;
       LevelManager.instance.OnLevelPaused -= HandleLevelPaused;
+      LevelManager.instance.OnLevelResumed -= HandleLevelResumed;
     }
   }
 
   private void HandleLevelStarted(BaseLevel level) {
+    Debug.Log("STARTED LEVEL");
     timerManager.ResetTimer();
     timerManager.StartTimer();
-    navigationManager.OpenMenu("Gameplay");
-    Debug.Log("STARTED LEVEL");
+    navigationManager.OpenMenu("System/None");
+    navigationManager.OpenMenu("Gameplay/Gameplay");
   }
 
   private void HandleLevelEnded(BaseLevel level) {
     Debug.Log($"Completed level {level.levelName} in {timerManager.GetTime():F2} seconds");
     timerManager.StopTimer();
-    navigationManager.OpenMenu("LevelCompleteMenu");
+    LevelManager.instance.ResumeLevel();
+    navigationManager.OpenMenu("System/LevelCompleteMenu");
+    navigationManager.OpenMenu("Gameplay/None");
+    navigationManager.Focus(completeLevelFocus);
   }
 
   private void HandleLevelPaused(BaseLevel level) {
-    timerManager.PauseTimer();
     Debug.Log("PAUSED LEVEL");
-    navigationManager.OpenMenu("PauseMenu");
+    timerManager.PauseTimer();
+    navigationManager.OpenMenu("System/PauseMenu");
+    StartCoroutine(DelayFocus(pausedLevelFocus));
+  }
+
+  private void HandleLevelResumed(BaseLevel level) {
+    timerManager.ResumeTimer();
+    navigationManager.OpenMenu("System/None");
+    Debug.Log("RESUMED LEVEL");
+  }
+
+
+  private IEnumerator DelayFocus(GameObject gameObject) {
+    yield return null;
+    navigationManager.Focus(gameObject);
+
+    Debug.Log("FOCUSING " + gameObject.name);
   }
   
 }
