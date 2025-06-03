c64539abdccabc9780c58a4eb6856e424b2263cb
Twaish <axiomeverything@gmail.com>
2025-06-03 13:19:09 +0200

    Added PlayerInputPauseHandler

---
+++ Assets/Scripts/Player/Input/PlayerInputPauseHandler.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputPauseHandler : MonoBehaviour {
+  private PlayerInput playerInput;
+
+  private void Awake() {
+    playerInput = GetComponent<PlayerInput>();
+    if (playerInput == null) {
+      Debug.LogError("PlayerInputPauseHandler: PlayerInput not found");
+      enabled = false;
+      return;
+    }
+
+    LevelManager.instance.OnLevelPaused += DisableInput;
+    LevelManager.instance.OnLevelResumed += EnableInput;
+  }
+
+  private void OnDestroy() {
+    LevelManager.instance.OnLevelPaused -= DisableInput;
+    LevelManager.instance.OnLevelResumed -= EnableInput;
+  }
+
+  private void DisableInput(BaseLevel _) {
+    playerInput.DeactivateInput();
+  }
+  private void EnableInput(BaseLevel _) {
+    playerInput.ActivateInput();
+  }
+}
